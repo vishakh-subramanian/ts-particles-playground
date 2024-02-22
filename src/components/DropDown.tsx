c1e9d4d43fb9c8af11d9633d30a2657bac1df7ed
vis-sub <vishakhs@gmail.com>
2024-02-22 16:42:56 -0500

    File Structure changes

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -4,11 +4,13 @@ import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import React from "react";
 import "./DropDown.css";
-
+import { optionMap } from "../utils/OptionMap";
 interface DropDownProps {
   currOptions: string;
   setCurrOptions: React.Dispatch<React.SetStateAction<string>>;
 }
+
+const getKeyString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 export const DropDown: React.FC<DropDownProps> = ({
   currOptions,
@@ -16,103 +18,6 @@ export const DropDown: React.FC<DropDownProps> = ({
 }) => {
   const [option1, setOption1] = React.useState("");
   const [option2, setOption2] = React.useState("");
-  const getKeyString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  const optionMap: { [key: string]: { value: number; label: string }[] } = {
-    A: [
-      { value: 1, label: "Absorbers" },
-      { value: 2, label: "Among Us" },
-    ],
-    B: [
-      { value: 101, label: "Background Mask" },
-      { value: 102, label: "Basic" },
-      { value: 103, label: "Big" },
-      { value: 104, label: "Black Hole" },
-      { value: 105, label: "Bubble" },
-    ],
-    C: [
-      // { value: 201, label: "Cards" },
-      { value: 201, label: "Chars" },
-      // { value: 202, label: "Click Confetti" },
-      { value: 202, label: "Click Pause" },
-      { value: 203, label: "Collisions Absorb" },
-      { value: 204, label: "Collisions Bounce" },
-      { value: 205, label: "Collisions Destroy" },
-      { value: 206, label: "Color Animation" },
-      { value: 207, label: "Curl Noise" },
-    ],
-    D: [
-      { value: 301, label: "Data Images" },
-      { value: 302, label: "Delay Color" },
-      { value: 303, label: "Delay Opacity" },
-      { value: 304, label: "Delay" },
-      { value: 305, label: "Delay Size" },
-      { value: 306, label: "Delay Stroke Color" },
-      { value: 307, label: "Destroy" },
-      { value: 308, label: "Disappearing" },
-      { value: 309, label: "Div Events" },
-    ],
-    E: [
-      { value: 401, label: "Effect Bubble" },
-      { value: 402, label: "Effect Trail" },
-      // { value: 403, label: "Emitter Absorber" },
-      // { value: 404, label: "Emitter Angled" },
-      // { value: 405, label: "Emitter Image Shape" },
-      // { value: 406, label: "Emitter Images" },
-      { value: 403, label: "Emitter" },
-      // { value: 408, label: "Emitter Paths" },
-      // { value: 409, label: "Emitter Shapes" },
-      // { value: 410, label: "Emitter Spawn Color" },
-      // { value: 411, label: "Emitter Text Shape" },
-      // { value: 412, label: "Emitter Text Stroke Shape" },
-    ],
-    F: [
-      // { value: 501, label: "Fireworks 2" },
-      // { value: 502, label: "Fireworks" },
-      { value: 501, label: "Fontawesome" },
-      { value: 502, label: "Forward" },
-    ],
-    G: [
-      { value: 601, label: "Gifs" },
-      { value: 602, label: "Grab Random Color" },
-      { value: 603, label: "Gradients" },
-      // { value: 604, label: "Gravity" },
-      // { value: 605, label: "Growing" },
-    ],
-    H: [
-      // { value: 701, label: "Hexagon Path" },
-      { value: 701, label: "Hollow Knight" },
-      // { value: 703, label: "Hyperspace" },
-    ],
-    I: [
-      { value: 801, label: "Image Mask" },
-      { value: 802, label: "Images Directions" },
-      { value: 803, label: "Images" }, // images has bugs
-      // { value: 804, label: "Infection "},
-    ],
-    J: [],
-    K: [],
-    L: [
-      { value: 1101, label: "Life" },
-      { value: 1102, label: "Light Hover" },
-      { value: 1103, label: "Link Triangles" },
-      { value: 1104, label: "Local Polygon Mask" },
-    ],
-    M: [],
-    N: [],
-    O: [],
-    P: [],
-    Q: [],
-    R: [],
-    S: [],
-    T: [],
-    U: [],
-    V: [],
-    W: [],
-    X: [],
-    Y: [],
-    Z: [],
-  };
 
   const handleChange = (event: SelectChangeEvent) => {
     setOption1(event.target.value);
