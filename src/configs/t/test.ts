5e841edf946ce39a82cd7a6a2a55e40679edf6f9
vis-sub <vishakhs@gmail.com>
2024-02-22 11:49:32 -0500

    formatting changes to config

--- src/configs/t/test.ts
+++ src/configs/t/test.ts
@@ -1,38 +1,38 @@
 import { ISourceOptions } from "tsparticles-engine";
 
 const options: ISourceOptions = {
-    key: "test",
-    name: "Test",
-    fullScreen: {
-        enable: false,
+  key: "test",
+  name: "Test",
+  fullScreen: {
+    enable: false,
+  },
+  background: {
+    color: "#000",
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
     },
-    background: {
-        color: "#000",
+    size: {
+      value: 6,
     },
-    particles: {
-        color: {
-            value: "#ffffff",
-        },
-        size: {
-            value: 6,
-        },
-        links: {
-            enable: true,
-            distance: 200,
-        },
-        number: {
-            value: 80,
-            density: {
-                enable: true,
-            },
-        },
-        opacity: {
-            value: 0.5,
-        },
-        shape: {
-            type: "circle",
-        },
+    links: {
+      enable: true,
+      distance: 200,
     },
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+      },
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+  },
 };
 
 export default options;
